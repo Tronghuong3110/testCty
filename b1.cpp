196f08d17e284fc8e8994d7950be7588898c5823
Tronghuong3110 <tronghuong2002@gmail.com>
2023-06-16 14:34:09 +0700

    commit

--- b1.cpp
+++ b1.cpp
@@ -1,6 +1,27 @@
-#include<bits/stc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+int solve(string s) {
+	bool check = true;
+	int pos;
+	while(check) {
+		check = false;
+		pos = s.find("AB");
+		if(pos != -1) {
+			s.erase(pos, 2);
+			check = true;
+		}
+		pos = s.find("CD");
+		if(pos != -1) {
+			s.erase(pos, 2);
+			check = true;
+		}
+	}
+	return s.length();	
+}
+
 int main() {
-    cout << "Hello" << "\n";
+	string s;
+	cin >> s;
+	cout << solve(s);
 }
